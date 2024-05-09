e4b7d8d7623f168ab74cb4e64aa18ab1ce3d7198
TecnoDespegue <157995252+Rene-Kuhm@users.noreply.github.com>
2024-05-08 22:24:08 -0300

    se optimizo el bundle

--- next.config.js
+++ next.config.js
@@ -1,4 +1,37 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
+const nextConfig = {
+    future: {
+      webpack5: true, // Habilita Webpack 5
+    },
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.optimization.minimize = true; // Minimiza el código en producción
+        config.optimization.splitChunks = {
+          cacheGroups: {
+            default: false,
+            vendors: false,
+            // Divide el código en chunks más pequeños
+            vendor: {
+              name: 'vendor',
+              chunks: 'all',
+              test: /[\\/]node_modules[\\/]/,
+              priority: 20,
+            },
+            common: {
+              name: 'common',
+              minChunks: 2,
+              chunks: 'all',
+              priority: 10,
+              reuseExistingChunk: true,
+              enforce: true,
+            },
+          },
+        };
+      }
+  
+      return config;
+    },
+  };
+  
+  module.exports = nextConfig;
+  
