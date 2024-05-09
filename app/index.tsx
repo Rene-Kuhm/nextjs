9a9cd6da4bf0e10db2456b989269de4304507933
TecnoDespegue <157995252+Rene-Kuhm@users.noreply.github.com>
2024-05-08 22:05:56 -0300

    se borro cosas innecesarias 7

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+// En index.ts
+export { Footer } from '@/src/layout/footer/v1';
+export { MainHeader } from '@/src/layout/header';
+export { AboutSection } from '@/src/sections/about/v1';
+export { ContactSection } from '@/src/sections/contact/v1';
+export { CtaSection } from '@/src/sections/cta/v1';
+export { Hero } from '@/src/sections/hero/v1';
+export { ProjectSection } from '@/src/sections/project/v1';
+export { PricingSection } from '@/src/sections/pricing/version-1';
+export { ServiceSection } from '@/src/sections/service/v1';
+export { StatisticsSection } from '@/src/sections/statistics/v1';
+export { TeamSection } from '@/src/sections/team/v2';
+export { TestimonialSection } from '@/src/sections/testimonial/v1';
