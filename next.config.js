b3ebc7d1710199bf59b29ac6f2e482bb3995b5a5
TecnoDespegue <157995252+Rene-Kuhm@users.noreply.github.com>
2024-05-08 22:35:21 -0300

    se optimizo imagenes

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+        path: '/_next/image',
+        loader: 'default',
+        deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+        imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+        disableStaticImages: true,
+      },
     future: {
       webpack5: true, // Habilita Webpack 5
     },
